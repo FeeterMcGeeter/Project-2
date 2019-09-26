5d23fcfcd6e8730f61f94597dede5170d1db9f97
Oscar Miranda <omiranda0058@gmail.com>
2019-09-25 21:53:02 -0500

    added code to models messages.js and user.js. Also added packages for passport and bcrypt.js and included requrements to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var session = require("express-session");
+var passport = require("./config/passport");
 
 var db = require("./models");
 
@@ -11,6 +13,13 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
@@ -23,6 +32,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
+require("./routes/userApiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
