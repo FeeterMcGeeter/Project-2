1e042a48e2f32a900574f30b1d8a8fe2a5bf2a3c
Jeffrey Davis <davisj0214@yahoo.com>
2019-09-30 21:40:48 -0500

    committing stuff

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,21 +2,30 @@ var db = require("../models");
 
 module.exports = function(app) {
     // Load index page
-    app.get("/", function(req, res) {
-        db.User.findAll({}).then(function(dbUser) {
-            res.render("login", {
-                msg: "Welcome!",
-                user: dbUser
-            });
-        });
+    app.get("/login", function(req, res) {
+        res.render("login");
     });
+
+    app.get("/login", function(req, res) {
+        if (req.user) {
+            res.redirect("/home");
+        } else {
+            res.redirect("/login");
+        }
+    });
+    
+    app.get("/home", function(req, res) {
+        res.render("home");
+    });
+
+    // app.get("/login", function(req, res) {
+    //     res.render("login");
+    // });
 
     // Load user page and pass User by id
     app.get("/user/:id", function(req, res) {
-        db.User.findOne({ where: { id: req.params.id } }).then(function(
-            dbUser
-        ) {
-            res.render("user", {
+        db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
+            res.render("home", {
                 user: dbUser
             });
         });
